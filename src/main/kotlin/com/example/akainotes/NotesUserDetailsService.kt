ab8c24b9ca3e75ededda166473e6e1644d10956c
Wojtek <krkezc@gmail.com>
2021-04-02 20:48:05 +0200

    user details service

---
+++ src/main/kotlin/com/example/akainotes/NotesUserDetailsService.kt
@@ -0,0 +1,18 @@
+package com.example.akainotes
+
+import kotlinx.coroutines.runBlocking
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class NotesUserDetailsService(private val usersRepository: UsersRepository) : UserDetailsService {
+    override fun loadUserByUsername(username: String?): UserDetails = runBlocking {
+        if (username == null) throw UsernameNotFoundException("Empty email")
+
+        return@runBlocking usersRepository.findUserByEmail(username)
+            ?: throw UsernameNotFoundException("$username not found")
+    }
+
+}
--- src/main/kotlin/com/example/akainotes/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/akainotes/WebSecurityConfig.kt
@@ -1,29 +1,38 @@
 package com.example.akainotes
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
 
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
-    @Throws(Exception::class)
-    override fun configure(http: HttpSecurity) {
-        http.cors().and().csrf().disable()
+class WebSecurityConfig(private val userDetailsService: NotesUserDetailsService) : WebSecurityConfigurerAdapter() {
+//    @Throws(Exception::class)
+//    override fun configure(http: HttpSecurity) {
+//        http.cors().and().csrf().disable()
+//    }
+
+//    @Bean
+//    fun corsConfigurationSource(): CorsConfigurationSource {
+//        val configuration = CorsConfiguration()
+//        configuration.allowedOrigins = listOf("*")
+//        configuration.allowedMethods = listOf("*")
+//        configuration.allowedHeaders = listOf("*")
+//        configuration.allowCredentials = true
+//        val source = UrlBasedCorsConfigurationSource()
+//        source.registerCorsConfiguration("/**", configuration)
+//        return source
+//    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailsService)
     }
 
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("*")
-        configuration.allowedMethods = listOf("*")
-        configuration.allowedHeaders = listOf("*")
-        configuration.allowCredentials = true
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
     }
 }
