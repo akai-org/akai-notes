c57bfb21978b5a2576cac6dc8460d3b589c091f5
Wojtek <krkezc@gmail.com>
2021-04-02 20:44:35 +0200

    user class extends user details

--- src/main/kotlin/com/example/akainotes/UsersRepository.kt
+++ src/main/kotlin/com/example/akainotes/UsersRepository.kt
@@ -3,4 +3,6 @@ package com.example.akainotes
 import com.example.akainotes.models.User
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface UsersRepository : CoroutineCrudRepository<User, String>
+interface UsersRepository : CoroutineCrudRepository<User, String> {
+    suspend fun findUserByEmail(email: String): User?
+}
--- src/main/kotlin/com/example/akainotes/models/User.kt
+++ src/main/kotlin/com/example/akainotes/models/User.kt
@@ -2,11 +2,30 @@ package com.example.akainotes.models
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 @Document
 data class User(
     @Id
     var id: String = "",
     val email: String,
-    val password: String,
-)
+    val pswd: String,
+    val role: String = ""
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf()
+    }
+
+    override fun getPassword(): String = pswd
+
+    override fun getUsername(): String = email
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
